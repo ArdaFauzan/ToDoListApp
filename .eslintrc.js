fe11f06f53e4c5b96caa30e052892f556cde1500
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-05-29 16:35:12 +0700

    Fixing dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['import'],
   rules: {
     'react-native/no-inline-styles': 0,
+    'import/no-unused-modules': [1, {unusedExports: true}],
+    'import/no-unresolved': 'error',
   },
 };
